523c42f16175d52a938cd99caa03650e8cc362f3
jpbusti <jpbustillo@uninorte.edu.co>
2025-09-11 21:56:58 +0000

    a

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -2,6 +2,7 @@ import Nodo
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def prepend(self, data):
         new_node = Nodo.Node(data)
@@ -10,5 +11,8 @@ class SinglyLinkedList:
 
 sll= SinglyLinkedList()
 sll.prepend(1)
+sll.prepend(1)
 sll.prepend(2)
+sll.prepend(3)
+sll.prepend(5)
 print(sll)
